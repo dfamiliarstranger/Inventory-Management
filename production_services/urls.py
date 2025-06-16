c9eea30bfef945d7299111be0d6549f3c001eef5
dfamiliarstranger <eniolaemmanuel@gmail.com>
2025-06-16 09:04:33 +0100

    corrected the inventory link

--- production_services/urls.py
+++ production_services/urls.py
@@ -6,7 +6,7 @@ from . import views
 urlpatterns = [ 
    path('raw-material-entry/', views.raw_material_entry, name='raw_material_entry'),
    path('raw-material-history/', views.raw_material_history, name='raw_material_history'),   
-   path('inventory_list/', views.inventory_list, name='inventory_list'),   
+   path('tp-inventory_list/', views.tp_inventory_list, name='tp_inventory_list'),   
    path('third-party-production_form/', views.create_production, name='third_party_production_form'),   
    path('tp_dashboard/', views.td_dashboard, name='third_party'),   
 ]
